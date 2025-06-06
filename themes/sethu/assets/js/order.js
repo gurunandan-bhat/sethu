f83e461d837997f27c6c945c637685ed5566c106
Gurunandan Bhat <gbhat@pobox.com>
2025-06-06 08:42:54 +0530

    Payment handling skeleton

--- themes/sethu/assets/js/order.js
+++ themes/sethu/assets/js/order.js
@@ -1,3 +1,7 @@
 import * as params from '@params';
 
-console.log('Generated with order.js', params);
+let button = document.getElementById('pay-now');
+let title = button.dataset.project;
+
+console.log('Generated with order.js', title);
+console.log('Order URL: ', params.orderURL);
